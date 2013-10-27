e2a439703c5272848aa49ac67c3f08f2c2a99a86
Arun Agrawal <arunagw@gmail.com>
2013-10-27 21:45:59 +0100

    Initial Commit

---
+++ TahDoodle/RRDocument.h
@@ -0,0 +1,13 @@
+//
+//  RRDocument.h
+//  TahDoodle
+//
+//  Created by Arun Agrawal on 27/10/13.
+//  Copyright (c) 2013 Arun Agrawal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RRDocument : NSDocument
+
+@end
---
+++ TahDoodle/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TahDoodle
+//
+//  Created by Arun Agrawal on 27/10/13.
+//  Copyright (c) 2013 Arun Agrawal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
